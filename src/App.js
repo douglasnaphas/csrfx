56a6c737382683d60f5fd01b912199dea671eb4f
SierraS <schellenbergsierra@gmail.com>
2019-10-19 16:30:18 -0400

    Set up initial view of actual page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import MyComponent from "./components/MyComponent";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant="h2" component="h1">
+        CSRFX
+      </Typography>
+      <Typography variant="h3" component="h3">
+        Attempt Cross-Site Request Forgeries to test defenses
+      </Typography>
+      <br />
+      <br />
+      <Paper className="White-paper">
+        <Typography variant="h5" component="h3">
+          Enter a URL
+        </Typography>
+        <br />
+        <TextField label="URL:"></TextField>
+      </Paper>
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>header</h1>
+      <p>paragraph...</p>
+    </div>
+  );
+}
+
+export default MyComponent;
